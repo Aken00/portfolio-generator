3baf4eebf6cb7f8de2944755505e6a3049f510bb
AKen00 <ajken04@gmail.com>
2020-10-02 16:51:28 -0400

    inquirer prompt exercise

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+const inquirer = require('inquirer');
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 // // Notice the lack of parentheses around the `profileDataArr` parameter?
 // const printProfileData = profileDataArr => {
@@ -31,15 +53,3 @@
 
 // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
 
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
